cde400cf054eaeb4012a8e004afc39534a9bf9b0
jedrzej-grabski <grabski.dev@gmail.com>
2026-04-22 14:29:53 -0700

    0.1.1 release

--- Formula/shed.rb
+++ Formula/shed.rb
@@ -2,7 +2,7 @@ class Shed < Formula
   desc "Ephemeral Python workspaces powered by uv"
   homepage "https://github.com/jedrzej-grabski/shed"
   url "https://github.com/jedrzej-grabski/shed/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "UPDATE_AFTER_RELEASE"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "uv"
